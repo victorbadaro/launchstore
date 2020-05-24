21005593146d6c3217a263721d82b744db834575
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-05-23 22:10:18 -0300

    Form to create products

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 const { Router } = require('express')
+const ProductController = require('./app/controllers/ProductController')
 const routes = Router()
 
 routes.get('/', function(req, res) {
     return res.render('layout')
+})
+
+routes.get('/products/create', ProductController.create)
+
+routes.get('/ads/create', function(req, res) {
+    return res.redirect('/products/create')
 })
 
 module.exports = routes

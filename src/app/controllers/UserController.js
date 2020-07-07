5c4cad3e556781c9277f73787da366d9b848828a
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-07-06 23:46:46 -0300

    Register form

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,7 @@
 module.exports = {
-    registerForm(req, res) {},
+    registerForm(req, res) {
+        return res.render('user/register')
+    },
     post(req, res) {},
     show(req, res) {},
     update(req, res) {},

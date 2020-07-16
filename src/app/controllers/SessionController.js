0d7e840aa42fa51c19574c7af474b0cb55a1fd43
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-07-15 23:57:47 -0300

    Starting login form

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,7 +1,13 @@
 module.exports = {
-    loginForm(req, res) {},
+    loginForm(req, res) {
+        return res.render('session/index')
+    },
     login(req, res) {},
-    logout(req, res) {},
+    logout(req, res) {
+        req.session.destroy()
+
+        return res.redirect('/')
+    },
     forgotForm(req, res) {},
     resetForm(req, res) {},
     forgot(req, res) {},

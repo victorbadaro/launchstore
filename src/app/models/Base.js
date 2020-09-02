b09d0b923b7ff89ac5d41831ed26ab7a40fb4446
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-09-01 23:35:54 -0300

    Improving base model

--- src/app/models/Base.js
+++ src/app/models/Base.js
@@ -24,6 +24,45 @@ const Base = {
         const result = await db.query(query)
 
         return result.rows[0]
+    },
+    async create(fields) {
+        let keys = []
+        let values = []
+
+        try {
+            Object.keys(fields).map(key => {
+                keys.push(key)
+                values.push(fields[key])
+            })
+
+            const query = `INSERT INTO ${this.table} (${keys.join(',')}) VALUES (${values.join(',')}) RETURNING id`
+            const result = await db.query(query)
+
+            return result.rows[0].id
+        } catch (error) {
+            console.error(error)
+        }
+    },
+    async update(id, fields) {
+        try {
+            let update = []
+            
+            Object.keys(fields).map(key => {
+                const line = `${key} = '${fields[key]}'`
+
+                update.push(line)
+            })
+    
+            let query = `UPDATE ${this.table} SET ${update.join(',')} WHERE id = ${id}`
+    
+            await db.query(query)
+            return
+        } catch (error) {
+            console.error(error)
+        }
+    },
+    delete(id) {
+        return db.query(`DELETE FROM ${this.table} WHERE id = $1`, [id])
     }
 }
 
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -8,37 +8,37 @@ Base.init({ table: 'users' })
 
 module.exports = {
     ...Base,
-    async create(data) {
-        try {
-            const query = `
-                INSERT INTO users (
-                    name,
-                    email,
-                    password,
-                    cpf_cnpj,
-                    cep,
-                    address
-                ) VALUES ($1, $2, $3, $4, $5, $6)
-                RETURNING id`
+    // async create(data) {
+    //     try {
+    //         const query = `
+    //             INSERT INTO users (
+    //                 name,
+    //                 email,
+    //                 password,
+    //                 cpf_cnpj,
+    //                 cep,
+    //                 address
+    //             ) VALUES ($1, $2, $3, $4, $5, $6)
+    //             RETURNING id`
             
-            // HASH OF PASSWORD
-            const passwordHash = await hash(data.password, 8)
+    //         // HASH OF PASSWORD
+    //         const passwordHash = await hash(data.password, 8)
             
-            const values = [
-                data.name,
-                data.email,
-                passwordHash,
-                data.cpf_cnpj.replace(/\D/g, ''),
-                data.cep.replace(/\D/g, ''),
-                data.address
-            ]
+    //         const values = [
+    //             data.name,
+    //             data.email,
+    //             passwordHash,
+    //             data.cpf_cnpj.replace(/\D/g, ''),
+    //             data.cep.replace(/\D/g, ''),
+    //             data.address
+    //         ]
     
-            const result = await db.query(query, values)
-            return result.rows[0].id
-        } catch (error) {
-            console.error(error)
-        }
-    },
+    //         const result = await db.query(query, values)
+    //         return result.rows[0].id
+    //     } catch (error) {
+    //         console.error(error)
+    //     }
+    // },
     async update(id, fields) {
         let query = 'UPDATE users SET'
 
